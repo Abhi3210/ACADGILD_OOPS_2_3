9ececc45772db7c2fc7782fd9e6bee67694bacf9
Abhijit Samanta <abhijit.samanta92@gmail.com>
2017-09-07 00:21:30 +0530

    first commit

---
+++ src/Final_Class.java
@@ -0,0 +1,10 @@
+final class Final{}  
+  
+class Final_Class extends Bike{  
+  void run(){System.out.println("running safely with 100kmph");}  
+    
+  public static void main(String args[]){  
+	  Final_Class f= new Final_Class();  
+  f.run();  
+  }  
+}  
---
+++ src/Final_Variable.java
@@ -0,0 +1,10 @@
+class Final_Variable{  
+ final int speedlimit=90;//final variable  
+ void run(){  
+  speedlimit=400;  
+ }  
+ public static void main(String args[]){  
+	 Final_Variable obj=new  Bike9();  
+ obj.run();  
+ }  
+}
